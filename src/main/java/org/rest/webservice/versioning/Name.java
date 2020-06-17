d429d721f18fec3762a7e50a40c9ee2bb12f2976
shoaib doulatabad <shoaib.doulatabad@gmail.com>
2020-06-18 00:44:59 +0400

    added different types of versioning

---
+++ src/main/java/org/rest/webservice/versioning/Name.java
@@ -0,0 +1,12 @@
+package org.rest.webservice.versioning;
+
+import lombok.*;
+
+@Data
+@Getter@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Name {
+  private String firstName;
+  private String lastName;
+}
